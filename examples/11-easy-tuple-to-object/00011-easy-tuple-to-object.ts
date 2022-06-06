fd604235599593a72513c66fd72f49b225223d5a
chenxi24 <40349890+chenxi24@users.noreply.github.com>
2022-06-06 22:04:51 +0800

    feat: add 11-easy-tuple-to-object

---
+++ examples/11-easy-tuple-to-object/00011-easy-tuple-to-object.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type cases = [
+  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+]
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>
+
+
+// ============= Your Code Here =============
+// T[number] 根据下标索引获取对应的value 
+type TupleToObject<T extends readonly string[]> = {
+  [P in T[number]]: P
+}
+
+
+
+
